0463986a258bbf4ceb89b4a3d2e638d9dd8e5afb
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2022-06-23 17:31:18 -0300

    Projeto criado

---
+++ backend/src/test/java/com/felipeveiga/fvcatalog/FvcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipeveiga.fvcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FvcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
