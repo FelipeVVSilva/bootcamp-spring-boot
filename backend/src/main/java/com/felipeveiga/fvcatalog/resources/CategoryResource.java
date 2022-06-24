e2a893490a3d7845642c2c607628558cf41b8d69
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2022-06-24 15:12:20 -0300

    findAll e findById de Category

--- backend/src/main/java/com/felipeveiga/fvcatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/felipeveiga/fvcatalog/resources/CategoryResource.java
@@ -1,10 +1,41 @@
 package com.felipeveiga.fvcatalog.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.felipeveiga.fvcatalog.entities.dto.CategoryDTO;
+import com.felipeveiga.fvcatalog.services.CategoryService;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryResource {
 
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<CategoryDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<CategoryDTO> list = service.findAll(pageRequest);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
 }
--- backend/src/main/java/com/felipeveiga/fvcatalog/services/CategoryService.java
+++ backend/src/main/java/com/felipeveiga/fvcatalog/services/CategoryService.java
@@ -1,10 +1,34 @@
 package com.felipeveiga.fvcatalog.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.felipeveiga.fvcatalog.entities.Category;
+import com.felipeveiga.fvcatalog.entities.dto.CategoryDTO;
+import com.felipeveiga.fvcatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
 
+	@Autowired
+	private CategoryRepository repo;
 	
+	@Transactional(readOnly = true)
+	public Page<CategoryDTO> findAll(PageRequest pageRequest){
+		Page<Category> list = repo.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repo.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
+	}
 	
 }
