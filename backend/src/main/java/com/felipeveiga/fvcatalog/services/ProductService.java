6989b56c16ed4f63f3b3187ea2c2facb66996dc5
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2022-06-27 13:51:08 -0300

    Product insert, update, delete

--- backend/src/main/java/com/felipeveiga/fvcatalog/services/ProductService.java
+++ backend/src/main/java/com/felipeveiga/fvcatalog/services/ProductService.java
@@ -2,15 +2,23 @@ package com.felipeveiga.fvcatalog.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.felipeveiga.fvcatalog.entities.Category;
 import com.felipeveiga.fvcatalog.entities.Product;
+import com.felipeveiga.fvcatalog.entities.dto.CategoryDTO;
 import com.felipeveiga.fvcatalog.entities.dto.ProductDTO;
+import com.felipeveiga.fvcatalog.repositories.CategoryRepository;
 import com.felipeveiga.fvcatalog.repositories.ProductRepository;
+import com.felipeveiga.fvcatalog.services.exceptions.DatabaseException;
 import com.felipeveiga.fvcatalog.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,6 +26,8 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repo;
+	@Autowired
+	private CategoryRepository catRepository;
 	
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAll(PageRequest pageRequest){
@@ -31,5 +41,55 @@ public class ProductService {
 		Product entity = obj.orElseThrow(() -> new ObjectNotFoundException("Id not exist: " + id));
 		return new ProductDTO(entity, entity.getCategories());
 	}
+
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		copyDtoToEntity(dto, entity);
+		entity = repo.save(entity);
+		return new ProductDTO(entity, entity.getCategories());
+	}
 	
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		try {
+			Product entity = repo.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repo.save(entity);
+			return new ProductDTO(entity, entity.getCategories());
+		}
+		catch(EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Id not exist: " + id);
+		}
+	}
+
+	@Transactional
+	public void delete(Long id) {
+		try {
+			repo.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException("Id not exist: " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		
+		entity.setName(dto.getName());
+		entity.setPrice(dto.getPrice());
+		entity.setDescription(dto.getDescription());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setDate(dto.getDate());
+		
+		entity.getCategories().clear();
+		
+		for(CategoryDTO catDto : dto.getCategories()) {
+			Category category = catRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
+		}
+		
+	}
 }
