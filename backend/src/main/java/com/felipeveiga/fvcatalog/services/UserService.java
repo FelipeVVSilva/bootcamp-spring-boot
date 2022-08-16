d8d35d1b822a409a11232f48ac06ccab0e6958ee
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2022-08-16 11:02:04 -0300

    Implementando checklist do Spring Security

--- backend/src/main/java/com/felipeveiga/fvcatalog/services/UserService.java
+++ backend/src/main/java/com/felipeveiga/fvcatalog/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,8 +30,10 @@ import com.felipeveiga.fvcatalog.services.exceptions.DatabaseException;
 import com.felipeveiga.fvcatalog.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
 	private UserRepository repo;
 	@Autowired
@@ -94,5 +101,18 @@ public class UserService {
 			Role role = roleRepository.getOne(roleDto.getId());
 			entity.getRoles().add(role);
 		}	
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		User user = repo.findByEmail(email);
+		
+		if(email == null) {
+			logger.error("User not found: " + user);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("User found: " + user);
+		return user;
 	}
 }
